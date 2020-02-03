ee21be2a445e9f404eb34c4f6db9e815322b8eb5
Lars <lars.wittenbrink@gmail.com>
2020-02-03 22:03:33 +0100

    fixing colors and finishing MainActivity.java

--- app/src/main/java/lars/wittenbrink/halligalli/MainActivity.java
+++ app/src/main/java/lars/wittenbrink/halligalli/MainActivity.java
@@ -2,13 +2,45 @@ package lars.wittenbrink.halligalli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Deklaration der View Variablen
+    private Button buttonPlay;
+    private Button buttonSettings;
+
+    //Deklaration der SharedPreferences
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialisierung der View Variablen
+        buttonPlay = findViewById(R.id.buttonPlay);
+        buttonSettings = findViewById(R.id.buttonSettings);
+
+        //Initialisierung der SharedPreferences
+        sharedPreferences = getSharedPreferences("settings", 0);
+
+        //Setzen der Listener für die View Objekte
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
     }
 }
