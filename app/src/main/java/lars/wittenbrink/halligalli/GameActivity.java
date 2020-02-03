0c0762de236fae476d81831a4ba3a24a8ad585c7
Lars <lars.wittenbrink@gmail.com>
2020-02-03 22:30:24 +0100

    adding ImageButton and function to GameActivity.java

--- app/src/main/java/lars/wittenbrink/halligalli/GameActivity.java
+++ app/src/main/java/lars/wittenbrink/halligalli/GameActivity.java
@@ -2,13 +2,92 @@ package lars.wittenbrink.halligalli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
+
+    //Deklaration der View Variablen
+    private ImageButton imageButtonBell;
+
+    //Deklaration der SharedPreferences
+    private SharedPreferences sharedPreferences;
+
+    //Deklaration des Vibrators und des MediaPlayers
+    private Vibrator vibrator;
+    private MediaPlayer mediaPlayer;
+
+    //Deklaration anderer Variablen
+    private final int ONE_ICON = 5;
+    private final int TWO_ICON = 3;
+    private final int THREE_ICON = 3;
+    private final int FOUR_ICON = 2;
+    private final int FIVE_ICON = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //Initialisierung der View Variablen
+        imageButtonBell = findViewById(R.id.imageButtonBell);
+
+        //Initialisierung der SharedPreferences
+        sharedPreferences = getSharedPreferences("settings", 0);
+
+        //Initialisierung des Vibrators und des MediaPlayers
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        mediaPlayer = MediaPlayer.create(this, R.raw.ring);
+
+        //Setzen der Listener für die View Objekte
+        imageButtonBell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                try {
+                    mediaPlayer.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if(sharedPreferences.getBoolean("sounds", false))
+                    mediaPlayer.start();
+                if(sharedPreferences.getBoolean("vibrations", false))
+                    vibrator.vibrate(100);
+
+                Toast toast=Toast.makeText(getApplicationContext(),"Difficulty: " + sharedPreferences.getInt("difficulty", 0),Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Closing Activity")
+                .setMessage("Are you sure you want to close this activity?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
