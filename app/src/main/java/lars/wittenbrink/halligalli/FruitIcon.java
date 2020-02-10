dfac63def3a7db4d2a3d5a329c23ad44491f9c75
Lars <lars.wittenbrink@gmail.com>
2020-02-10 16:25:34 +0100

    modified

--- app/src/main/java/lars/wittenbrink/halligalli/FruitIcon.java
+++ app/src/main/java/lars/wittenbrink/halligalli/FruitIcon.java
@@ -3,13 +3,13 @@ package lars.wittenbrink.halligalli;
 public enum FruitIcon {
     ICONA(R.attr.iconA), ICONB(R.attr.iconB), ICONC(R.attr.iconC), ICOND(R.attr.iconD);
 
-    private final long id;
+    private final int id;
 
     FruitIcon(int id) {
         this.id = id;
     }
 
-    public long getValue() {
+    public int getId() {
         return id;
     }
 }
--- app/src/main/java/lars/wittenbrink/halligalli/GameActivity.java
+++ app/src/main/java/lars/wittenbrink/halligalli/GameActivity.java
@@ -3,17 +3,25 @@ package lars.wittenbrink.halligalli;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -38,6 +46,25 @@ public class GameActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
 
     //Deklaration anderer Variablen
+    private Player player1;
+    private Player player2;
+
+    //Deklaration der Test Variablen
+    private TextView player1openText;
+    private TextView player1closeText;
+    private TextView player2openText;
+    private TextView player2closeText;
+    private ImageView player11;
+    private ImageView player12;
+    private ImageView player13;
+    private ImageView player14;
+    private ImageView player15;
+    private ImageView player21;
+    private ImageView player22;
+    private ImageView player23;
+    private ImageView player24;
+    private ImageView player25;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +84,30 @@ public class GameActivity extends AppCompatActivity {
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         mediaPlayer = MediaPlayer.create(this, R.raw.ring);
 
+        //Initialisierung anderer Variablen
+         player1 = new Player("Lars");
+         player2 = new Player("Justin");
+
+        //Initialisierung der Test Variablen
+        player1openText = findViewById(R.id.player1openText);
+        player1closeText = findViewById(R.id.player1closeText);
+        player2openText = findViewById(R.id.player2openText);
+        player2closeText = findViewById(R.id.player2closeText);
+        player11 = findViewById(R.id.player11);
+        player12 = findViewById(R.id.player12);
+        player13 = findViewById(R.id.player13);
+        player14 = findViewById(R.id.player14);
+        player15 = findViewById(R.id.player15);
+        player21 = findViewById(R.id.player21);
+        player22 = findViewById(R.id.player22);
+        player23 = findViewById(R.id.player23);
+        player24 = findViewById(R.id.player24);
+        player25 = findViewById(R.id.player25);
+
+         //Karten zuweisen
+        distributeCards(mixCards(createCards()));
+        refreshUI();
+
         //Setzen der Listener für die View Objekte
         imageButtonBell.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,8 +122,7 @@ public class GameActivity extends AppCompatActivity {
                     mediaPlayer.start();
                 if (sharedPreferences.getBoolean("vibrations", false))
                     vibrator.vibrate(100);
-                Toast toast = Toast.makeText(getApplicationContext(), "Difficulty: " + sharedPreferences.getInt("difficulty", 0), Toast.LENGTH_SHORT);
-                toast.show();
+
             }
         });
     }
@@ -119,8 +169,7 @@ public class GameActivity extends AppCompatActivity {
     }
 
 
-
-    public static List<Card> createCards() {
+    public List<Card> createCards() {
         List<Card> cards = new LinkedList<>();
         for (FruitIcon fruitIcon : FruitIcon.values()) {
             for (FruitNumber fruitNumber : FruitNumber.values()) {
@@ -132,16 +181,69 @@ public class GameActivity extends AppCompatActivity {
         return cards;
     }
 
-    public static List<Card> mixCards(List<Card> cards) {
+    public List<Card> mixCards(List<Card> cards) {
         Collections.shuffle(cards);
         return cards;
     }
 
-    public static void main(String[] args) {
-        List<Card> cards = mixCards(createCards());
-        for (Card card : cards) {
-            System.out.println(card.getFruitIcon() + " " + card.getFruitNumber());
+    public void distributeCards(List<Card> cards){
+        while(!cards.isEmpty()){
+            player1.addClosedCard(cards.get(cards.size()-1));
+            cards.remove(cards.size()-1);
+            player2.addClosedCard(cards.get(cards.size()-1));
+            cards.remove(cards.size()-1);
         }
-        System.out.println(cards.size());
+    }
+
+    public void refreshUI(){
+        if(player1.getOpenedCard()!=null)
+        switch(player1.getOpenedCard().getFruitNumber()){
+            case FIVE:
+                player12.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+                player14.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+            case THREE:
+                player15.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+                player11.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+            case ONE:
+                player13.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+                break;
+            case FOUR:
+                player12.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+                player14.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+            case TWO:
+                player11.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+                player15.setImageResource(getResIdFromAttribute(this, player1.getOpenedCard().getFruitIcon().getId()));
+        }
+        if(player2.getOpenedCard()!=null)
+            switch(player1.getOpenedCard().getFruitNumber()){
+                case FIVE:
+                    player22.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+                    player24.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+                case THREE:
+                    player25.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+                    player21.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+                case ONE:
+                    player23.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+                    break;
+                case FOUR:
+                    player22.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+                    player24.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+                case TWO:
+                    player21.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+                    player25.setImageResource(getResIdFromAttribute(this, player2.getOpenedCard().getFruitIcon().getId()));
+            }
+        player1closeText.setText(Integer.toString(player1.getClosedStack().size()));
+        player1openText.setText(Integer.toString(player1.getOpenedStack().size()));
+        player2closeText.setText(Integer.toString(player2.getClosedStack().size()));
+        player2openText.setText(Integer.toString(player2.getOpenedStack().size()));
+    }
+
+    //Methode, um die Ressourcen ID aus Attributen zu bekommen
+    public static int getResIdFromAttribute(final Activity activity, final int attr) {
+        if(attr==0)
+            return 0;
+        final TypedValue typedvalueattr=new TypedValue();
+        activity.getTheme().resolveAttribute(attr,typedvalueattr,true);
+        return typedvalueattr.resourceId;
     }
 }
